1c5dbfe401ab9523c10106988521cdf57406212c
jordani.rozon <jordani.rozon@ehana.com>
2020-09-06 12:17:48 -0400

    Removi funciones no necesarias

--- genmed-api/Utils/Extensions/Extensiones.cs
+++ genmed-api/Utils/Extensions/Extensiones.cs
@@ -122,29 +122,5 @@ namespace genmed_api.Utils.Extensions
         }
     
         #endregion
-
-        public static byte[] ObjectToByteArray(this Object obj)
-        {
-            if(obj == null)
-                return null;
-
-            BinaryFormatter bf = new BinaryFormatter();
-            MemoryStream ms = new MemoryStream();
-            bf.Serialize(ms, obj);
-
-            return ms.ToArray();
-        }
-
-        // Convert a byte array to an Object
-        public static Object ByteArrayToObject(this byte[] arrBytes)
-        {
-            MemoryStream memStream = new MemoryStream();
-            BinaryFormatter binForm = new BinaryFormatter();
-            memStream.Write(arrBytes, 0, arrBytes.Length);
-            memStream.Seek(0, SeekOrigin.Begin);
-            Object obj = (Object) binForm.Deserialize(memStream);
-
-            return obj;
-        }
     }
 }
